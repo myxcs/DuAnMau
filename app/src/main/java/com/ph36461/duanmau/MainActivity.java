694de1e03f66775b763c4290ffa5411bf588e127
myxcs <not.quangthai.1806@gmail.com>
2024-03-14 07:31:10 +0700

    Initial commit

---
+++ app/src/main/java/com/ph36461/duanmau/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ph36461.duanmau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
